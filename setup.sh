27cb10f747a30c9be3b2e5be96346c4e0f4a91af
Andrew-Fox <andrew.fox@forcepoint.com>
2019-01-30 12:53:14 -0600

    simple setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ $(whoami) != 'root' ]; then
+   echo "Must be root to run script"
+   exit 1
+fi
+
+cd /usr/local/bin
+wget https://raw.githubusercontent.com/afpacket/okta-aws-helper-scripts/master/okta-login.sh
+wget https://raw.githubusercontent.com/afpacket/okta-aws-helper-scripts/master/okta-setenv.sh
