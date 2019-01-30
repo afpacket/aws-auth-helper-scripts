16b4d256b62baa14cae0f1c2a1ca765c84977d5a
Andrew-Fox <andrew.fox@forcepoint.com>
2019-01-30 12:55:19 -0600

    update setup script

--- setup.sh
+++ setup.sh
@@ -6,5 +6,9 @@ if [ $(whoami) != 'root' ]; then
 fi
 
 cd /usr/local/bin
+
 wget https://raw.githubusercontent.com/afpacket/okta-aws-helper-scripts/master/okta-login.sh
 wget https://raw.githubusercontent.com/afpacket/okta-aws-helper-scripts/master/okta-setenv.sh
+
+chmod 755 ./okta-login.sh
+chmod 755 ./okta-setenv.sh
