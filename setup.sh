ca3866d5e94789653ff51fe93411013b237a57b3
Andrew Fox <afpacket@users.noreply.github.com>
2020-04-06 22:20:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,9 +7,9 @@ fi
 
 cd /usr/local/bin
 
-cp ./okta-login.sh /usr/local/bin
-cp ./aws-native-mfa-login.sh /usr/local/bin
-cp ./aws-setenv.sh /usr/local/bin
+cp okta-login.sh /usr/local/bin
+cp aws-native-mfa-login.sh /usr/local/bin
+cp aws-setenv.sh /usr/local/bin
 ln -s /usr/local/bin/okta-setenv.sh /usr/local/bin/aws-setenv.sh
 
 chmod 755 /usr/local/bin/okta-login.sh
