1408248d0e18603963cd533a12091aad4f963bd1
Andrew Fox <afpacket@users.noreply.github.com>
2020-04-06 22:19:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,12 +7,12 @@ fi
 
 cd /usr/local/bin
 
-curl -L https://raw.githubusercontent.com/afpacket/okta-aws-helper-scripts/master/okta-login.sh -O
-curl -L https://raw.githubusercontent.com/afpacket/okta-aws-helper-scripts/master/aws-native-mfa-login.sh -O
-curl -L https://raw.githubusercontent.com/afpacket/okta-aws-helper-scripts/master/aws-setenv.sh -O
+cp ./okta-login.sh /usr/local/bin
+cp ./aws-native-mfa-login.sh /usr/local/bin
+cp ./aws-setenv.sh /usr/local/bin
 ln -s /usr/local/bin/okta-setenv.sh /usr/local/bin/aws-setenv.sh
 
-chmod 755 ./okta-login.sh
-chmod 755 ./aws-native-mfa-login.sh
-chmod 755 ./aws-setenv.sh
+chmod 755 /usr/local/bin/okta-login.sh
+chmod 755 /usr/local/bin/aws-native-mfa-login.sh
+chmod 755 /usr/local/bin/aws-setenv.sh
 ln -s /usr/local/bin/aws-setenv.sh /usr/local/bin/okta-setenv.sh
